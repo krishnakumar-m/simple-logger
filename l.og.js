a81a42db5843a92d4bee8ffa90be3d6af161078f
Krishnakumar Muraleedharan <krishnakumarm24386@gmail.com>
2016-05-24 18:10:04 +0530

    Create l.og.js

---
+++ l.og.js
@@ -0,0 +1,71 @@
+var l = {
+enable : true,
+ert : function(msg) {
+ if(l.enable) {
+  alert(msg);
+ }
+},
+og : function(msg) {
+if(l.enable) {
+	showMsg(msg);
+ }
+}
+};
+
+function showMsg(msg) {
+ var msgObj = {
+ 	type : 'pre',
+  content : msg
+ };
+ if(document.getElementById('log-div')) {
+ document.getElementById('log-div').style.display = 'block';
+ document.getElementById('log-div').appendChild(newElm(msgObj));
+  
+ } else {
+ var minBox = {
+    type : 'div',
+    id : 'log-min',
+    content : ':'
+  };
+  document.body.appendChild(newElm(minBox));
+   var closeBox = {
+    type : 'div',
+    id : 'log-close',
+    content : 'X'
+  };
+  var clearBox = {
+    type : 'div',
+    id : 'log-clear',
+    content : 'Clear'
+  };
+  var logDivObj = {
+   type : 'div',
+   id : 'log-div',
+   content : [closeBox,clearBox,msgObj]
+  };
+  document.body.appendChild(newElm(logDivObj));
+  document.getElementById('log-close').addEventListener('click',function(){
+   document.getElementById('log-div').style.display = 'none';
+     document.getElementById('log-min').style.display = 'block';
+  });
+  
+    document.getElementById('log-clear').addEventListener('click',function(){
+   var logdiv = document.getElementById('log-div');
+   var msgs = logdiv.getElementsByTagName('pre');
+// NodeList so fix like this
+while(msgs[0]) { 
+logdiv.removeChild(msgs[0]);
+}
+  });
+  
+    document.getElementById('log-min').addEventListener('click',function(){
+   document.getElementById('log-min').style.display = 'none';
+    document.getElementById('log-div').style.display = 'block';
+  });
+  
+ }
+ 
+ var element = document.getElementById('log-div');
+element.scrollTop = element.scrollHeight;
+ 
+}
